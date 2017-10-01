374ed2628ab116018e23d746f936aabb20c8962a
Spiralwise <contact@spiralwise.net>
2017-10-01 16:56:30 +0200

    Distance computation
    Including all obstacles

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -172,22 +172,39 @@ public class HexGrid : MonoBehaviour {
 		for (int i = 0; i < cells.Length; i++)
 			cells [i].Distance = int.MaxValue;
 		WaitForSeconds delay = new WaitForSeconds (1 / 60f);
-		Queue<HexCell> openSet = new Queue<HexCell> ();
+		List<HexCell> openSet = new List<HexCell> ();
 		cell.Distance = 0;
-		openSet.Enqueue (cell);
+		openSet.Add (cell);
 		while (openSet.Count > 0) {
 			yield return delay;
-			HexCell current = openSet.Dequeue ();
+			HexCell current = openSet [0];
+			openSet.RemoveAt (0);
 			for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
 				HexCell neighbor = current.GetNeighbor (d);
-				if (neighbor == null || neighbor.Distance != int.MaxValue)
+				if (neighbor == null)
 					continue;
 				if (neighbor.IsUnderWater)
 					continue;
-				if (current.GetEdgeType (neighbor) == HexEdgeType.Cliff)
+				HexEdgeType edgeType = current.GetEdgeType (neighbor);
+				if (edgeType == HexEdgeType.Cliff)
 					continue;
-				neighbor.Distance = current.Distance + 1;
-				openSet.Enqueue (neighbor);
+
+				int distance = current.Distance;
+				if (current.HasRoadThroughEdge (d))
+					distance += 1;
+				else if (current.Walled != neighbor.Walled)
+					continue;
+				else {
+					distance += edgeType == HexEdgeType.Flat ? 5 : 10;
+					distance += neighbor.UrbanLevel + neighbor.FarmLevel + neighbor.PlantLevel;
+				}
+				
+				if (neighbor.Distance == int.MaxValue) {
+					neighbor.Distance = distance;
+					openSet.Add (neighbor);
+				} else if (distance < neighbor.Distance)
+					neighbor.Distance = distance;
+				openSet.Sort ((x, y) => x.Distance.CompareTo (y.Distance));
 			}
 		}
 	}
