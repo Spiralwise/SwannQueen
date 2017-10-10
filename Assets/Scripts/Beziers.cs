771d290c817a3088c69bce5bde775ea7a582903d
Spiralwise <contact@spiralwise.net>
2017-10-10 18:40:30 +0200

    Smooth unit movement animation

---
+++ Assets/Scripts/Beziers.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Beziers {
+
+	public static Vector3 GetPoint (Vector3 a, Vector3 b, Vector3 c, float t) {
+		float r = 1f - t;
+		return r * r * a + 2f * r * t * b + t * t * c;
+	}
+}
